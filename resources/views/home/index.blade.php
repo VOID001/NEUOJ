91a5bad4ffb72dbfbf3d034cf6f1eef2c003d681
Jianqiu Zhang <zhangjianqiu_133@yeah.net>
2015-11-28 13:40:15 +0800

    views: Add home view

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,18 @@
+@if(!isset($noLogin))
+    <div>Welcome {{ $username }} !</div>
+    <div>You are currently logged in</div>
+    <a href="/dashboard">Dashboard</a>
+    <a href="/logout">logout</a>
+@else
+    <div>Welcome Guest!</div>
+    <div>You are currently not logged in</div>
+    <a href="/auth/signin">Sign in</a>
+    <a href="/auth/signup">Sign Up</a>
+@endif
+
+<a href="/problem">Problem</a>
+<a href="/status">Status</a>
+<a href="/discuss">Discuss Board</a>
+
+<h3>Announcement</h3>
+<div>Something here</div>
