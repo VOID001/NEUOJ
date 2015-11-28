8bc6e9bc03a136d9c516bbd4e1a1b03d5b7c55cf
Jianqiu Zhang <zhangjianqiu_133@yeah.net>
2015-11-28 09:49:04 +0800

    seeds: Add and enable UserTableSeeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends DatabaseSeeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $userObject = new User;
+
+        $users = [
+            [
+                "username" => "VOID001",
+                "password" => Hash::make('1234567'),
+                "email" => "zhangjianqiu_133@yeah.net"
+            ],
+            [
+                "username" => "admin",
+                "password" => Hash::make("admin"),
+                "email" => "admin@noreply.com"
+            ]
+        ];
+
+        foreach($users as $user)
+        {
+            User::create($user);
+        }
+    }
+}
