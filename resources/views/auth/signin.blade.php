e54807c398ff3d06093a5bc4c922d19fd9a2db41
Jianqiu Zhang <zhangjianqiu_133@yeah.net>
2015-11-28 13:38:08 +0800

    views/auth: Add views
    signin: Add errmsg
    singup: created

---
+++ resources/views/auth/signin.blade.php
@@ -0,0 +1,33 @@
+@if(count($errors) > 0 || isset($loginError))
+    <div>
+        @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        @if(isset($loginError))
+            <li>{{ $loginError }}</li>
+        @endif
+    </div>
+@endif
+<form action="/auth/signin" method="POST">
+{{ csrf_field() }}
+    <table>
+        <th>Sign in</th>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" name="username" value="{{ old('username') }}"/></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="pass"/></td>
+            </tr>
+            <tr>
+                <td><div>Forget Password? Click <a href="/auth/request">here</a> to reset</div></td>
+            </tr>
+            <tr>
+                <td><div>Not signed up? <a href="/auth/signup">Sign Up Now!</a></div></td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Sign in"></td>
+            </tr>
+    </table>
+</form>
---
+++ resources/views/auth/signup.blade.php
@@ -0,0 +1,21 @@
+<form action="/auth/signin" method="POST">
+    <table>
+        <th>Sign in</th>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" id="userName"/></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="text" id="pass"/></td>
+            </tr>
+            <tr>
+                <td>Re-type Password</td>
+                <td><input type="password" id="confirmpass"/></td>
+            </tr>
+            <tr>
+                <td>Email Address</td>
+                <td><input type="text" id="email"/></td>
+            </tr>
+    </table>
+</form>
