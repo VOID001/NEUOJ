8490b345e8b5c5e43731804be69efa6318b85c1b
Jianqiu Zhang <zhangjianqiu_133@yeah.net>
2015-11-28 09:48:07 +0800

    routes: Add top-level routes and auth sub-routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,56 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [
+    "as" => "home",
+    "uses" => "HomeController@showHome",
+]);
+
+Route::get('/auth', function(){
+    return "This is the authenticate root";
 });
+
+Route::get('/profile', function(){
+    return "This is the profile root";
+});
+
+Route::get('/dashboard', function(){
+    return "This is the dashboard root";
+});
+
+Route::get('/problem', function(){
+    return "This is the problem root";
+});
+
+Route::get('/status', function(){
+    return "This is the status root";
+});
+
+Route::get('/contest', function(){
+    return "This is the contest root";
+});
+
+Route::get('/discuss', function(){
+    return "This is the BBS root";
+});
+
+Route::match(['post','get'], '/auth/signin', [
+    "as" => "signin",
+    //"middleware" => "",
+    "uses" => "AuthController@loginAction"
+]);
+
+/*
+ * These routes below are only use for testing and debugging
+ * DO NOT ENABLE IT IN THE PRODUCTION PROJECT
+ */
+
+Route::get('/test/getss',[
+    "as" => "test.getss",
+    "uses" => "TestController@getSession"
+]);
+
+Route::get('/test/delss',[
+    "as" => "test.getss",
+    "uses" => "TestController@destroySession"
+]);
