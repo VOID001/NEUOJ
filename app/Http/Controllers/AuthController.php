5545ef27d5016586f8dab734bf07d86817738319
Jianqiu Zhang <zhangjianqiu_133@yeah.net>
2015-11-28 13:37:13 +0800

    controller/AuthController: add validation and error-handling to AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use App\User;
@@ -11,24 +12,46 @@ use Illuminate\Support\Facades\Redirect;
 
 class AuthController extends Controller
 {
+    /*
+     * @params $request
+     * @comment handle GET & POST requests
+     * and validate the post data then proceed login action
+     *
+     * @return View or Redirect
+     */
     public function loginAction(Request $request)
     {
         $input = [];
+        $data = [];
         if($request->method() == 'POST')
         {
             $input = $request->input();
-            var_dump($input);
-            $userObject = new User;
-            $row = $userObject->where('username',$input['userName'])->firstOrFail();
-            $passHash = $row->password;
-            if(Hash::check($input['pass'], $passHash))
+            $vdtor = Validator::make($input, [
+                "username" => "required|max:255",
+                "pass" => "required"
+            ]);
+            if($vdtor->fails())
             {
-                $request->session()->put('userName',$row->username);
-                return Redirect::route('home');
+                return Redirect::route('signin')->withErrors($vdtor)->withInput($input);
+            }
+            $userObject = new User;
+            $row = $userObject->where('username',$input['username'])->first();
+            if(isset($row))
+            {
+                $passHash = $row->password;
+                if (Hash::check($input['pass'], $passHash))
+                {
+                    $request->session()->put('username', $row->username);
+                    return Redirect::route('home');
+                }
+                $data['loginError'] = "Invalid Password";
+            }
+            else
+            {
+                $data['loginError'] = "No such user!";
             }
         }
-
-        return View::make('auth.signin');
+        return View::make('auth.signin', $data);
     }
 }
 
