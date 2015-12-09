55a99ebf04586ce41233466c6ba92d5216eb1a0f
Jianqiu Zhang <zhangjianqiu_133@yeah.net>
2015-12-09 19:30:39 +0800

    database/migrations: Create UserInfo table

---
+++ database/migrations/2015_12_09_192719_CreateUserInfoTable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('userinfo', function (Blueprint $table) {
+            $table->increments("info_id");
+            $table->integer("uid")->unique();
+            $table->string("school");
+            $table->string("stu_id");
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("userinfo");
+    }
+}
