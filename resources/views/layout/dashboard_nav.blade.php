baa5533341c4dd4a0610293e5be975f14a817c93
su123456 <sujinming0125@163.com>
2015-12-22 22:27:36 +0800

    views/layout.dashboard_nav: Add View

---
+++ resources/views/layout/dashboard_nav.blade.php
@@ -0,0 +1,16 @@
+<div class="col-xs-2 dashboard_nav" >
+    <div class="dashboard_top">
+        <div class="text-center"><img class="dashboard_logo" src="/image/neuacmlogo.PNG"/></div>
+        <div class="dashboard_username text-center">username</div>
+        <hr class="dashboard_hr"/>
+    </div>
+    <ul class="nav nav-pills nav-stacked" data-spy="affix" data-offset-top="125">
+
+        <li class="active"><a href="#section-1">Problem</a></li>
+        <li><a href="#section-2">Contest</a></li>
+        <li><a href="#section-3">Submission</a></li>
+        <li class="separate-item"></li>
+        <li><a href="#section-4">Setting</a></li>
+        <li><a href="#section-5">Profile</a></li>
+    </ul>
+</div>
