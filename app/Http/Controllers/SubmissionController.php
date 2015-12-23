efdec063ff222e41d5d4d9423fa150c51df65da1
Jianqiu Zhang <zhangjianqiu_133@yeah.net>
2015-12-23 11:05:27 +0800

    Enable Contest Submission Function
    controller/SubmissionController: Now can submit to contest, with a
    unique filename, then redirect to contest status page(not finished)
    
    views/errors.contest_end: If Contest not running ,redirect here

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -13,6 +13,9 @@ use App\Http\Controllers\AuthController;
 use App\Problem;
 use App\User;
 use App\Submission;
+use App\ContestProblem;
+use App\ContestUser;
+use App\Contest;
 
 class SubmissionController extends Controller
 {
@@ -31,8 +34,8 @@ class SubmissionController extends Controller
             $vdtor = Validator::make($request->all(),[
                 "code" => "required|min:50|max:50000",
             ]);
-            var_dump($request->input());
-            var_dump($request->session()->all());
+            //var_dump($request->input());
+            //var_dump($request->session()->all());
             $uid = $request->session()->get('uid');
             $fileName = "";
             $submission = new Submission;
@@ -135,7 +138,59 @@ class SubmissionController extends Controller
 
     public function contestSubmitAction(Request $request, $contest_id, $problem_id)
     {
-        return "Submitted!";
+        if($request->method() == "POST")
+        {
+            $data = [];
+
+            $contestProblemObj = ContestProblem::where([
+                'contest_id' => $contest_id,
+                'contest_problem_id' => $problem_id
+            ])->first();
+            $contestObj = Contest::where('contest_id', $contest_id)->first();
+            $data['contest'] = $contestObj;
+            if(time() < strtotime($contestObj->begin_time) || time() > strtotime($contestObj->end_time))
+            {
+                return View::make("errors.contest_end", $data);
+            }
+
+            $realProblemID = $contestProblemObj->problem_id;
+
+            $langsufix = [
+                "C" => "c",
+                "Java" => "java",
+                "C++11" => "cc",
+                "C++" => "cpp",
+            ];
+
+            $vdtor = Validator::make($request->all(),[
+                "code" => "required|min:50|max:50000",
+            ]);
+            var_dump($request->input());
+            var_dump($request->session()->all());
+            $uid = $request->session()->get('uid');
+            $fileName = "";
+            $submission = new Submission;
+            if($vdtor->fails())
+            {
+                return Redirect::to($request->server('HTTP_REFERER'))
+                    ->withErrors($vdtor);
+            }
+            $fileName = $contest_id . "-" . $uid."-".$problem_id."-".time().".".$langsufix[$request->input('lang')];
+            echo $fileName;
+            Storage::put("submissions/".$fileName, $request->input('code'));
+            $submission->pid = $realProblemID;
+            $submission->uid = $uid;
+            $submission->cid = $contest_id;
+            $submission->lang = $request->input('lang');
+            $submission->result = "Pending";
+            $submission->submit_time = date('Y-m-d-H:i:s');
+            $submission->submit_file = $fileName;
+            $submission->md5sum = md5($request->input('code'));
+            $submission->judge_status = 0;
+            $submission->save();
+            $runid = $submission->id;
+            //return Redirect::to("/contest/$contest_id/status/$runid");
+        }
     }
 
 }
---
+++ resources/views/errors/contest_end.blade.php
@@ -0,0 +1,6 @@
+<h1>Contest Ended!</h1>
+
+<div>
+    You are not allowed to submit now<br/>
+    Click <a href="/contest/{{ $contest->contest_id }}">Here</a> to back to contest Page
+</div>
