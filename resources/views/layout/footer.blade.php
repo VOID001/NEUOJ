146599bf80d6bcad611c8d6ebe4d8a7e396a9963
DYS1230 <dongyesen@gmail.com>
2016-02-24 13:59:06 +0800

    views/layout/footer:Add version number

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,4 @@
 <footer role="contentinfo" class="text-center footer">
         <span class="footspanclass">Anything about the Problems, Please Contact Admin:admin<br>
-        All Copyright&copy;Reserved 2013-2017 NEU-ACM TEAM<span>
+        All Copyright&copy;Reserved 2013-2017 NEU-ACM TEAM&nbsp;&nbsp;&nbsp;Ver 0.8</span>
 </footer>
-
-
