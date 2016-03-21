797281eb364059b604396c87673029c50873815e
Catsworld <519418441@qq.com>
2016-03-21 16:52:35 +0800

    Edit the header to be more cool and ADD "POLYMER" STYLE!!
    public/css/main.css: Change and add many styles.
    views/layout.header: Change the navbar to Polymer style.
    views/layout.head: Add the head declaration of Polymer.
    views/layout.footer: Add a random function to display the code.

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -7,3 +7,6 @@
 <link rel="stylesheet" href="/css/bootstrap.css">
 <script src="/js/jquery.js"></script>
 <script src="/js/bootstrap.js"></script>
+<script src="/bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
+<link rel="import" href="/bower_components/paper-button/paper-button.html">
+<link rel="import" href="/bower_components/paper-dropdown-menu/paper-dropdown-menu.html">
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,7 +2,7 @@
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
 <header role="banner" >
-    <div class="navbar navbar-default" role="navigation" style="background: #336699">
+    <div class="navbar navbar-default" role="navigation" style="background-color: #3f51b5">
         <div class="container">
             <div class="navbar-header">
                 <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-responsive-collapse">
@@ -15,22 +15,30 @@
             </div>
             <div class="collapse navbar-collapse navbar-responsive-collapse">
                 <ul class="nav navbar-nav">
-                    <li id="home"><a href="/" >Home</a></li>
-                    <li id="problem"><a href="/problem">Problem</a></li>
-                    <li id="status"><a href="/status">Status</a></li>
-                    <li id="contest"><a href="/contest">Contest</a></li>
-                    <li id="discuss"><a href="#">Discuss</a></li>
-                    <li id="rating"><a href="#">Rating</a></li>
-                    <li id="temp_"><a href="/dashboard/profile">Dashboard</a></li>
+                    <li id="home"><a href="/"><paper-button>Home<div class="activebar"></div></paper-button></a></li>
+                    <li id="problem"><a href="/problem"><paper-button>Problem<div class="activebar"></div></paper-button></a></li>
+                    <li id="status"><a href="/status"><paper-button>Status<div class="activebar"></div></paper-button></a></li>
+                    <li id="contest"><a href="/contest"><paper-button>Contest<div class="activebar"></div></paper-button></a></li>
+                    <li id="discuss"><a href="#"><paper-button>Discuss<div class="activebar"></div></paper-button></a></li>
+                    <li id="rating"><a href="#"><paper-button>Rating<div class="activebar"></div></a></paper-button></li>
                 </ul>
 
                 @if(Request::session()->get('username')!="")
-                    <a href="/dashboard" class="text-right" id="dashboard" ><nobr>{{Request::session()->get('username')}}</nobr></a>
-                    <a href="/auth/logout" class="btn btn-info" id="logout">logout</a>
+                    <div class="btn-group" id="dashboard">
+                        <button class="btn btn-grey" onclick="javascript:window.location.href='/dashboard'">{{Request::session()->get('username')}}</button>
+                        <button class="btn btn-grey dropdown-toggle btn-caret" type="button" data-toggle="dropdown""><span class="caret"></span></button>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li><a href="/dashboard"><img src="/avatar/{{Request::session()->get('uid')}}" style="width:118px;height:118px;"/></a></li>
+                            <li role="presentation" class="divider"></li>
+                            <li><a href="/dashboard">Dashboard</a></li>
+                            <li role="presentation" class="divider"></li>
+                            <li><a href="/auth/logout">Log Out</a></li>
+                        <ul>
+                    </div>
                 @else
-                    <a href="/auth/ssologin" class="btn btn-info" id="ssologin">SSO</a>
-                    <a href="/auth/signin" class="btn btn-info" id="signin">Sign in</a>
-                    <a href="/auth/signup" class="btn btn-info" id="signup">Sign Up</a>
+                    <a href="/auth/ssologin" id="ssologin"><paper-button raised  class="mdbtn-white">SSO</paper-button></a>
+                    <a href="/auth/signin" id="signin"><paper-button raised class="mdbtn-white">Sign in</paper-button></a>
+                    <a href="/auth/signup" id="signup"><paper-button raised class="mdbtn-white">Sign Up</paper-button></a>
                 @endif
 
 
