5ff42567502849d5b2ebdaf4f8fa0ce5909fe8b5
onionwyl <onionwyl@qq.com>
2016-04-02 18:40:41 +0800

    Init Training functions database/migrations: add two tables Controllers/TrainingController: init functions routes: init training routes views/training: init training routers

---
+++ app/Http/Controllers/TrainingController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+
+use App\Train;
+use App\TrainProblem;
+use Problem;
+use App\Submission;
+use App\User;
+use App\Userinfo;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\Controller;
+
+class TrainingController extends Controller
+{
+    public function showTrainingDashboard(Request $request)
+    {
+        
+    }
+
+    public function addTraining(Request $request)
+    {
+
+    }
+
+    public function deleteTraining(Requset $request)
+    {
+
+    }
+
+    public function setTraining(Request $request)
+    {
+
+    }
+
+    public function getTrainingList(Request $request)
+    {
+
+    }
+
+    public function getTrainingByID(Request $request)
+    {
+
+    }
+
+    public function getTrainingByIDByChepter(Request $request)
+    {
+
+    }
+}
---
+++ database/migrations/2016_04_02_150922_CreateTrainProblemTable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrainProblemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('train_problem', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('train_id');
+            $table->integer('problem_id');
+            $table->integer('chepter_id');
+            $table->integer('train_problem_id');
+            $table->string('problem_title');
+            $table->integer('problem_level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('train_problem');
+    }
+}
