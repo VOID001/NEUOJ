d54521f9c223910f5ac4d1a0bdf4ba08aaa80b2b
sjm <sujinming0125@163.com>
2016-04-05 20:22:09 +0800

    Update Signin design
    public/css/main.css: abstract the class about form
    auth.signin: change the style of signin

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -12,41 +12,49 @@
     	});
     </script>
 </head>
-<body>
-    @include("layout.header")
+<body style="background-color: #d2d6de">
+<br>
+<br>
+<h2 class="text-center">Sign in</h2>
+<br>
+<br>
 <div align="center">
-<form action="/auth/signin" method="POST">
-{{ csrf_field() }}
-    <h3 class="text-center">Sign in</h3>
-    <div style="height: 20px">
-        @if(count($errors) > 0 )
-            <div class="form-group" style="width: 400px;text-align: left"><div class="label label-warning" style="font-size: 13px">{{$errors->all()[0]}}</div></div>
-        @elseif(isset($loginError))
-            <div class="form-group" style="width: 400px;text-align: left"><div class="label label-warning" style="font-size: 13px">{{ $loginError }}</div></div>
-        @endif
+    <div class="signin_box">
+        <p class="signin_box_msg">Sign in to start your code</p>
+        <form action="/auth/signin" method="POST">
+        {{csrf_field()}}
+            <div class="form-group signin_box_userbox">
+                <span class="glyphicon glyphicon-user signin_box_gly"></span>
+                <input type="text" name="username" value="@if(!isset($username)){{ old('username') }}@else{{ $username }}@endif" class="form-control signin_box_user" placeholder="username"tabindex="1">
+            </div>
+            <div class="form-group signin_box_userbox ">
+                <span class="glyphicon glyphicon-lock signin_box_gly"></span>
+                <input type="password" name="pass" class="form-control signin_box_user" placeholder="password"tabindex="2">
+            </div>
+            <div class="form-group signin_box_userbox signin_box_cptbox">
+                <span class="glyphicon glyphicon-exclamation-sign signin_box_gly"></span>
+                <input type="text" name="captcha" class="form-control signin_box_user" placeholder="captcha" tabindex="3"/>
+                <img src="{{ captcha_src("flat") }}" class="signin_box_cpt_p" alt="Captcha" id="captcha"/>
+            </div>
+            <div class="row signin_box_row signin_box_warning">
+                @if(count($errors) > 0 )
+                    <span class="label label-danger" style="font-size: 12px"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;{{$errors->all()[0]}}</span>
+                @elseif(isset($loginError))
+                    <span class="label label-danger" style="font-size: 12px"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;{{ $loginError }}</span>
+                @endif
+            </div>
+            <div class="row signin_box_row">
+                <input type="submit" value="Sign in" style="padding-top: 3px" class="btn btn-info pull-right">
+            </div>
+        </form>
+        <br>
+        <div class="row" style="width: 80%">
+            <a href="/auth/request" class="pull-left">I forget my password</a>
+            <br/>
+            <a href="/auth/signup" class="pull-left">Register a new membership</a>
+        </div>
+
     </div>
-    <table id="signinTable">
-        <tr >
-            <td style="padding-left: 100px">Username</td>
-            <td ><input type="text" name="username" class="form-control" value="@if(!isset($username)){{ old('username') }}@else{{ $username }}@endif" tabindex="1"/></td>
-            <td><div style="padding-left: 4px"><a href="/auth/signup">Sign Up Now!</a></div></td>
-        </tr>
-        <tr>
-            <td style="padding-left: 100px">Password</td>
-            <td ><input type="password" name="pass"class="form-control" tabindex="2"/></td>
-            <td><div style="padding-left: 4px">Click <a href="/auth/request">here</a> to reset</div></td>
-        </tr>
-            <td style="padding-left: 100px">Captcha(Ignore Case)</td>
-            <td><input type="text" name="captcha" class="form-control"  tabindex="3"/></td>
-            <td><img src="{{ captcha_src("flat") }}" alt="Captcha" id="captcha"/></td>
-        <tr>
-        </tr>
-        <tr>
-            <td></td>
-            <td class="text-right"><input type="submit" value="Sign in" class="btn btn-info"></td>
-        </tr>
-    </table>
-</form>
     <!--
 <div class="panel panel-default" style="width: 400px;height: 80px">
     <div class="panel-heading text-left">第三方登陆</div>
@@ -57,6 +65,5 @@
     </div>
 </div>
     -->
-    @include("layout.footer")
 </body>
 </html>
