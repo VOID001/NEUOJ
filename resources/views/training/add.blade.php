7b025a080ed96bae3858fc7d09bc9f574a4d9357
DYS1230 <dongyesen@gmail.com>
2016-04-20 13:16:32 +0800

    Update training design
    public/css/main.css:Update css
    views/layout/dashboard_nav:Add training nav
    views/layout/header:Add training nav
    views/training/add:Update design
    views/training/dashboard:Update design
    views/training/index:Update design
    views/training/list:Update design

--- resources/views/training/add.blade.php
+++ resources/views/training/add.blade.php
@@ -1,12 +1,30 @@
+<!doctype html>
+<html>
+<head>
+    <title>Add Training</title>
+    @include("layout.head")
+    <link rel="stylesheet" href="/css/main.css">
+    <script type="text/javascript">
+        $(function(){
+            $("#dashboard_training").addClass("dashboard_subnav_active");
+        })
+    </script>
+</head>
+<body>
+@include("layout.dashboard_nav")
+<div class="col-xs-10">
+<h3 class="text-center">Add Training</h3>
 @foreach($errors->all() as $error)
         <div>
             <ul>{{ $error }}</ul>
         </div>
 @endforeach
-<form action="/dashboard/training/add" method='post'>
+<form id="contest_add_form" class="form-inline" action="/dashboard/training/add" method='post'>
 {{ csrf_field() }}
 <label>Train Name</label>
 <input type = "text" name = "train_name"/>
+<label>Description</label>
+<input type = "text" name = "description"/>
 <div id = "add_contest">
 <a href="javascript:addChapter()">Add Chapter</a>
 <a href="javascript:deleteChapter()">Delete The Last Chapter</a>
@@ -16,21 +34,25 @@
 <input type="submit" value="Submit"/>
 </form>
 
+</div>
+</body>
+</html>
+
 
 
 
 
 
 <script language="javascript">
-	var chapterCount = 1;
-	var problemCount = 0;
-	function addChapter()
-	{
-		var chapterItem = "<div id = 'chapter_" + chapterCount + "'><br>Chapter" + chapterCount + "<a href = \"javascript:addProblem(" + chapterCount + ")\">Add Problem</a><br></div>";
-		document.getElementById("add_contest").insertAdjacentHTML("beforeEnd", chapterItem);
+    var chapterCount = 1;
+    var problemCount = 0;
+    function addChapter()
+    {
+        var chapterItem = "<div id = 'chapter_" + chapterCount + "'><br>Chapter" + chapterCount + "<a href = \"javascript:addProblem(" + chapterCount + ")\">Add Problem</a><br></div>";
+        document.getElementById("add_contest").insertAdjacentHTML("beforeEnd", chapterItem);
         document.getElementById("train_chapter").value = chapterCount;
-		chapterCount++;
-	}
+        chapterCount++;
+    }
 
     function deleteChapter()
     {
@@ -39,17 +61,17 @@
         document.getElementById("train_chapter").value = chapterCount-1;
     }
 
-	function addProblem(chapter_id)
-	{
-		var problemItem =  "<div id = p_" + problemCount + "><label>Problem ID</label>"+
-						   "<input type = \"hidden\" name = \"problem_chapter[]\" value = " + chapter_id + "/>"+
-						   "<input type = \"text\" name = \"problem_id[]\" />"+
-						   "<label>Problem Name</label>"+
-						   "<input type = \"text\" name = \"problem_name[]\"/>"+
+    function addProblem(chapter_id)
+    {
+        var problemItem =  "<div id = p_" + problemCount + "><label>Problem ID</label>"+
+                           "<input type = \"hidden\" name = \"problem_chapter[]\" value = " + chapter_id + "/>"+
+                           "<input type = \"text\" name = \"problem_id[]\" />"+
+                           "<label>Problem Name</label>"+
+                           "<input type = \"text\" name = \"problem_name[]\"/>"+
                            "<a href=\"javascript:deleteProblem("+chapter_id+","+problemCount+")\">Delete Problem</a><br></div>";
-		document.getElementById("chapter_"+chapter_id).insertAdjacentHTML("beforeEnd", problemItem);
-		problemCount++;
-	}
+        document.getElementById("chapter_"+chapter_id).insertAdjacentHTML("beforeEnd", problemItem);
+        problemCount++;
+    }
 
     function deleteProblem(chapter_id, div_id)
     {
--- resources/views/training/index.blade.php
+++ resources/views/training/index.blade.php
@@ -1,27 +1,90 @@
-{{ $training->train_name }}
-<p>
-{{ $training->description }}
-<p>
-{{ $training->train_chapter }}
-<p>
-{{ $chapter_in }}
-<br>
+<!doctype html>
+<html>
+<head>
+<title>Training</title>
+@include("layout.head")
+<link rel="stylesheet" href="/css/main.css">
+<script type="text/javascript">
+$(function(){
+	$('#training').addClass('active');
+	$('.panel-heading').click(function(){
+		var iconChildren = $(this).children().eq(-1);
+		if(iconChildren.hasClass('glyphicon-chevron-down')){
+			iconChildren.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+		}else if(iconChildren.hasClass('glyphicon-chevron-up')){
+			iconChildren.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+		};
+	});
+	$('.list_hover').has('i').css('background','#D9EDF7');
+})
+</script>
+</head>
+<body class="body_grey">
+@include("layout.header")
+	<div class="container-fluid" id="training_container">
+		<img src="/avatar/2" class="img_fit">
+		<div class="training_title">
+			<div>
+				<h1>{{ $training->train_name }}</h1>
+				<p>{{ $training->description }}</p>
+			</div>
+			<hr/>
+			<div>
+				@for($i = 1; $i <= $chapter_in -1; $i++)
+					<i class="glyphicon glyphicon-star star-color"></i>
+				@endfor
+				@for($i = 1; $i <= $training->train_chapter - $chapter_in + 1; $i++)
+					<i class="glyphicon glyphicon-star-empty star-color"></i>
+				@endfor
+				&nbsp;&nbsp;&nbsp;&nbsp;{{ $chapter_in -1 }} / {{ $training->train_chapter }}
+			</div>
+		</div>
+	</div>
+<div  class="panel-group training_group" id="accordion" aria-multiselectable="true">
 @for($i = 1; $i <= $training->train_chapter; $i++)
-	chapter{{ $i }}
-	<br>
-	@foreach($chapter[$i] as $problem)
-		@if($i <= $chapter_in)
-		<a href = '/problem/{{ $problem->problem_id }}'> {{ $problem->title }} </a>
-		{{ $problem->problem_id }}
-		{{ $problem->title }}
-		@if($problem->ac)
-		ac
-		@endif
-		<br>
-		@else
-		{{ $problem->problem_id }}
-		{{ $problem->title }}
-		<br>
-		@endif
-	@endforeach
+<div class="panel panel-default">
+	@if($i <= $chapter_in)
+		<div class="panel-heading" data-toggle="collapse" href="#collapse_{{ $i }}" aria-expanded="true">
+			<span>chapter{{ $i }}</span>
+			<i class="glyphicon glyphicon-chevron-down glyphicon_right"></i>
+		</div>
+		<div id="collapse_{{ $i }}" class="panel-collapse collapse in">
+			<div class="panel-body">
+				<ul class="list-group">
+				@foreach($chapter[$i] as $problem)
+					<li class="list_hover list-group-item" onClick="window.location.href = '/problem/{{ $problem->problem_id }}';">
+						<span>ID : {{ $problem->problem_id }}&nbsp;&nbsp;&nbsp;&nbsp;TITLE : {{ $problem->title }}
+							@if($problem->ac)
+							<i class="glyphicon glyphicon-ok"></i>
+							@endif
+						</span>
+					</li>
+				@endforeach
+				</ul>
+			</div>
+	@else
+		<div class="panel-heading" data-toggle="modal" data-target="#information">
+			<span>chapter{{ $i }}</span>
+			<i class="glyphicon glyphicon-lock glyphicon_right"></i>
+		</div>
+		<div id="collapse_{{ $i }}" class="panel-collapse collapse">
+	@endif
+	</div>
+	</div>
 @endfor
+</div>
+<div class="modal fade" id="information" tabindex="-1">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">Information</div>
+			<div class="modal-body">Please  finish previous chapters to unlock this chapter</div>
+			<div class="modal-footer">
+			<button type="button" class="btn btn-default close" data-dismiss="modal">OK</button>
+			</div>
+		</div>
+	</div>
+</div>
+<div style="padding-bottom: 40px"></div>
+@include("layout.footer")
+</body>
+</html>
